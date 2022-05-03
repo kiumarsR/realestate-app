ddb4aff4f461a4b9f4f46a2ab3517d29029d5f80
kiumarsR <kiumarsrabiee@gmail.com>
2022-05-03 20:45:28 +0430

    route/web.php is updated

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use App\Http\Controllers\RealestateController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\test\ReControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,25 +20,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('welcome');
 Route::view('/404', 'errors.404');
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/login', [LoginController::class, 'login'])->name('login');
+Route::post('/authenticate', [LoginController::class, 'authenticate'])->name('authenticate');
+Route::get('/register', [LoginController::class, 'register'])->name('register');
+Route::post('/validation', [LoginController::class, 'store'])->name('store');
+Route::put('/user-update', [UserController::class, 'update'])->name('user.update');
+
+Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
+
 Route::prefix('/user')->group(function () {
     Route::get('', [UserController::class, 'index'])->name('profile');
+    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
     Route::get('/myproperties', [UserController::class, 'myProperties'])->name('my-properties');
     Route::get('/favourite-properties', [UserController::class, 'favourite'])->name('favourite-properties');
     Route::get('/add-property', [UserController::class, 'addProperty'])->name('add-properties');
 });
+
 Route::prefix('/properties')->group(function () {
     Route::get('', [RealestateController::class, 'properties'])->name('properties');
     Route::get('/add-property', [RealestateController::class, 'create'])->name('add-property');
     Route::get('/single', [RealestateController::class, 'single'])->name('single-property');
+    Route::post('/store', [RealestateController::class, 'store'])->name('properties.store');
 });
-// Route::prefix('/properties')->group(function () {
-//     Route::get('/', [RealestateController::class, 'properties'])->name('properties');
-//     Route::get('/single', [RealestateController::class, 'single'])->name('single-property');
-//     Route::get('/add-property', [RealestateController::class, 'create'])->name('add-property');
-// });
 
 Route::view('/contact', 'pages.contact')->name('contact');
 Route::view('/faq', 'pages.faq')->name('faq');
 Route::view('/about', 'pages.about')->name('about');
+
+Route::resources([
+    'tests' => ReControler::class,
+]);
